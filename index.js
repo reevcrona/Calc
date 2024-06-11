72687676c7f8fdc6f46df444dbe9394f9450665e
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-06-11 13:54:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,66 +13,65 @@ const inactiveCalc = document.querySelector(".inactive-calc");
 const mainContainer = document.querySelector(".miniräknare-container")
 
 const header = document.querySelector(".header");
+const miniHeader = document.querySelector(".mini-header");
 
-miniOne.addEventListener("click",() => {
-    miniOne.style.transform = 'scale(2)'
-    miniOne.classList.remove("hover");
-    miniTwo.style.transform = 'scale(0.4)'
-    miniThree.style.transform = "scale(0.4)"
-    inactiveCalc.appendChild(miniTwo)
-    inactiveCalc.appendChild(miniThree)
-    mainContainer.appendChild(miniOne)
-    header.style.display = "none";
-
-    miniTwo.classList.add("hover");
-    miniThree.classList.add("hover");
-})
+const myCalcs = document.querySelectorAll(".calc");
 
 
-miniTwo.addEventListener("click",() =>{
-    miniTwo.style.transform = 'scale(2)'
-    miniTwo.classList.remove("hover");
-    miniOne.style.transform = 'scale(0.4)'
-    miniThree.style.transform = "scale(0.4)"
-    inactiveCalc.appendChild(miniOne)
-    inactiveCalc.appendChild(miniThree)
-    mainContainer.appendChild(miniTwo)
-    header.style.display = "none";
-
-    miniOne.classList.add("hover");
-    miniThree.classList.add("hover");
-})
-
-miniThree.addEventListener("click",() =>{
-    miniThree.style.transform = 'scale(2)'
-    miniThree.classList.remove("hover");
-    miniOne.style.transform = 'scale(0.4)'
-    miniTwo.style.transform = "scale(0.4)"
-    inactiveCalc.appendChild(miniOne)
-    inactiveCalc.appendChild(miniTwo)
-    mainContainer.appendChild(miniThree)
-    header.style.display = "none";
-
-    miniOne.classList.add("hover");
-    miniTwo.classList.add("hover");
-})
-
-
-buttonsOne.forEach((button) => {
-    button.addEventListener("click",() => {
-       if(button.value === "C"){
-       inputOne.value = "";
-       }
-       else{
-        inputOne.value += button.value;
-       }
-        
+function checkActive(){
+    myCalcs.forEach((calc,index) => {
+        if(calc.classList.contains("active")){
+           if(index === 0 ){
+            miniOne.removeEventListener("click",handleMiniOneClick)
+            buttonsOne.forEach((button) => {
+                button.addEventListener("click",buttonListenerOne)
+            })
+           }else if(index ===1){
+            miniTwo.removeEventListener("click",handleMiniTwoClick)
+            buttonsTwo.forEach((button) => {
+                button.addEventListener("click",buttonListenerTwo)
+            })
+           }else if(index ===2){
+            miniThree.removeEventListener("click",handleMiniThreeClick);
+            buttonsThree.forEach((button) =>{
+                button.addEventListener("click",buttonListenerThree)   
+            })
+           }
+        }else if (calc.classList.contains("inactive")){
+            if(index === 0){
+                miniOne.addEventListener("click",handleMiniOneClick)
+                buttonsOne.forEach((button) => {
+                    button.removeEventListener("click",buttonListenerOne)
+                })
+            }else if(index ===1){
+                miniTwo.addEventListener("click",handleMiniTwoClick)
+                buttonsTwo.forEach((button) => {
+                    button.removeEventListener("click",buttonListenerTwo)
+                })
+            }else if(index ===2){
+                miniThree.addEventListener("click",handleMiniThreeClick)
+                buttonsThree.forEach((button) => {
+                    button.removeEventListener("click",buttonListenerThree)
+                })
+            }
+        }
     })
-})
+}
 
+function buttonListenerOne(event){
+    const button = event.target;
 
-buttonsTwo.forEach((button) => {
-    button.addEventListener("click",() => {
+    if(button.value === "C"){
+        inputOne.value = "";
+        }
+        else{
+         inputOne.value += button.value;
+        }
+}
+
+function buttonListenerTwo(event){
+    const button = event.target;
+
         if(button.value === "C"){
         inputTwo.value = "";
         }else if(button.value === "Del"){
@@ -81,19 +80,110 @@ buttonsTwo.forEach((button) => {
         else{
          inputTwo.value += button.value;
         }
-     })
-})
+     
+}
 
-buttonsThree.forEach((button) =>{
-    button.addEventListener("click",() => {
+function buttonListenerThree(event){
+    const button = event.target;
+
         if(button.value === "C"){
         inputThree.value = "";
         }
         else{
          inputThree.value += button.value;
         }
-     })
-})
+    
+}
+
+function handleMiniOneClick(){
+    miniOne.style.transform = 'scale(1.3)'
+    miniOne.classList.remove("hover");
+    miniTwo.style.transform = 'scale(0.4)'
+    miniThree.style.transform = "scale(0.4)"
+    inactiveCalc.appendChild(miniTwo)
+    inactiveCalc.appendChild(miniThree)
+    mainContainer.appendChild(miniOne)
+    header.style.display = "none";
+    inactiveCalc.style.display = "flex";
+    miniTwo.classList.add("hover");
+    miniThree.classList.add("hover");
+    
+
+   inputTwo.value ="";
+   inputThree.value ="";
+
+   miniHeader.style.opacity = "1";
+    myCalcs[0].classList.add("active");
+    myCalcs[1].classList.remove("active");
+    myCalcs[2].classList.remove("active");
+    myCalcs[2].classList.add("inactive")
+    myCalcs[1].classList.add("inactive")
+
+    checkActive();
+}
+
+function handleMiniTwoClick(){
+    miniTwo.style.transform = 'scale(1.3)'
+    miniTwo.classList.remove("hover");
+    miniOne.style.transform = 'scale(0.4)'
+    miniThree.style.transform = "scale(0.4)"
+    inactiveCalc.appendChild(miniOne)
+    inactiveCalc.appendChild(miniThree)
+    mainContainer.appendChild(miniTwo)
+    header.style.display = "none";
+    inactiveCalc.style.display = "flex";
+    miniOne.classList.add("hover");
+    miniThree.classList.add("hover");
+
+    inputOne.value ="";
+    inputThree.value ="";
+    
+    miniHeader.style.opacity = "1";
+    myCalcs[1].classList.add("active");
+    myCalcs[2].classList.remove("active");
+    myCalcs[0].classList.remove("active");
+    myCalcs[0].classList.add("inactive")
+    myCalcs[2].classList.add("inactive")
+
+    checkActive();
+}
+
+function handleMiniThreeClick(){
+    miniThree.style.transform = 'scale(1.3)'
+    miniThree.classList.remove("hover");
+    miniOne.style.transform = 'scale(0.4)'
+    miniTwo.style.transform = "scale(0.4)"
+    inactiveCalc.appendChild(miniOne)
+    inactiveCalc.appendChild(miniTwo)
+    mainContainer.appendChild(miniThree)
+    header.style.display = "none";
+    inactiveCalc.style.display = "flex";
+    miniOne.classList.add("hover");
+    miniTwo.classList.add("hover");
+
+    inputOne.value ="";
+    inputTwo.value ="";
+    
+    myCalcs[2].classList.add("active");
+    myCalcs[1].classList.remove("active");
+    myCalcs[0].classList.remove("active");
+    myCalcs[0].classList.add("inactive")
+    myCalcs[1].classList.add("inactive")
+    miniHeader.style.opacity = "1";
+    checkActive();
+}
+
+miniOne.addEventListener("click",handleMiniOneClick)
+miniTwo.addEventListener("click",handleMiniTwoClick)
+miniThree.addEventListener("click",handleMiniThreeClick)
+
+
+
+
+
+
+
+
 
 inputOne.addEventListener("input",limitInput)
 inputTwo.addEventListener("input",limitInput)
